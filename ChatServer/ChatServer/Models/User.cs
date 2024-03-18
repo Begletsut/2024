d1c76ac434c008966cad5a3958fe5beb1aab2873
Begletsut <b3gl3tsut@gmail.com>
2024-03-18 21:28:37 +0200

    Chat server
    for android app

---
+++ ChatServer/ChatServer/Models/User.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Net.Sockets;
+using System.Text;
+namespace ChatServer.Models
+{
+    
+    public class User
+    {
+
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Photo { get; set; }
+        public string LocationL { get; set; }
+        public string LocationA { get; set; }
+        public TcpClient Connection { get; set; }
+
+        public User(string name, int age, string photo, string locationL, string locationA, TcpClient connection)
+        {
+            Name = name;
+            Age = age;
+            Photo = photo;
+            LocationL = locationL;
+            LocationA = locationA;
+            Connection = connection;
+        }
+        public override string ToString()
+        {
+            return $"{Name}#{Age}#{LocationL}#{LocationA}#{Photo}";
+        }
+    }
+
+}
